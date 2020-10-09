357ae4b81ea029c6fb3c8ea736b4d0a9bd1043b9
jae hyeok <couhensoft@gmail.com>
2020-10-09 21:37:25 +0900

    algorithm 0만들기

--- src/Algorithm/Fastcampus/영만들기.java
+++ src/Algorithm/Fastcampus/영만들기.java
@@ -1,56 +1,116 @@
 package Algorithm.Fastcampus;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class 영만들기 {
     static int N;
-    static ArrayList<Character[]> list;
+    static ArrayList<Character[]> answerList;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int testCase = Integer.parseInt(st.nextToken());
 
-        list = new ArrayList<>();
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("js");
 
         for(int i=0;i<testCase;i++){
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
-            Character[] dataArr = new Character[N * 2 - 1];
+
+            Character[] dataArr = new Character[N-1];
+            ArrayList<Integer> intArr = new ArrayList<>();
+            answerList = new ArrayList<>();
 
             for(int j=0;j<N;j++){
-                dataArr[j*2] = (char)(j + 49);
+                intArr.add(j+1);
             }
 
-            fow( N-1, dataArr);
+            fow(0, dataArr);
 
-            for(Character[] c : list){
-                String str = "";
-                for(int k=0;k<N-1;k++){
-                    //str += ()
+            Collections.sort(answerList, new Comparator<Character[]>() {
+                        @Override
+                        public int compare(Character[] o1, Character[] o2) {
+                            String str1 = new String();
+                            String str2 = new String();
+
+                            for(int i=0;i<o1.length;i++){
+                                str1 += String.valueOf(o1[i]);
+                            }
+                            for(int i=0;i<o2.length;i++){
+                                str2 += String.valueOf(o2[i]);
+                            }
+
+                            return str1.compareTo(str2);
+                        }
+                    });
+
+            for(Character[] c : answerList){
+                //case start
+                String str = new String();
+                String strAnswer;
+                String calStrArr[];
+                ArrayList<Character> charStrArr = new ArrayList<>();
+
+                int sum = 0;
+
+                for(int j=0;j<c.length;j++){
+                    str += String.valueOf((j+1));
+                    str += c[j];
+                }
+                str += String.valueOf(N);
+
+                strAnswer = new String(str);
+                str = str.replaceAll(" ","");
+
+                //수정된 숫자 배열
+                calStrArr = str.split("\\+|\\-");
+
+                //공백 문자가 빠진 문자 배열
+                for(char ch : str.toCharArray()){
+                    if(ch == '+'){
+                        charStrArr.add(ch);
+                    }else if(ch == '-'){
+                        charStrArr.add(ch);
+                    }
+                }
+
+                sum = Integer.parseInt(calStrArr[0]);
+
+                for(int k=0;k<charStrArr.size();k++){
+                    if(charStrArr.get(k) == '+'){
+                        sum += Integer.parseInt(calStrArr[k+1]);
+                    }else if(charStrArr.get(k) == '-')
+                        sum -= Integer.parseInt(calStrArr[k+1]);
+                }
+
+                if(sum == 0){
+                    System.out.println(strAnswer);
                 }
             }
         }
-
     }
 
-    static void fow(int idx, Character[] charArr){
-        if(charArr.length == idx){
-            list.add(charArr.clone());
+    static void fow(int idx, Character c[]){
+        if(c.length == idx){
+            answerList.add(c.clone());
             return ;
         }
 
-        if(idx * 2 + 1 < N * 2 - 1){
-            charArr[idx * 2 + 1] = '+';
-            fow(idx * 2 + 1, charArr);
-            charArr[idx * 2 + 1] = '-';
-            fow(idx * 2 + 1, charArr);
-            charArr[idx * 2 + 1] = ' ';
-            fow(idx * 2 + 1, charArr);
+        if(idx < N - 1){
+            c[idx] = '+';
+            fow(idx + 1, c);
+            c[idx] = '-';
+            fow(idx + 1, c);
+            c[idx] = ' ';
+            fow(idx + 1, c);
         }
     }
 }
